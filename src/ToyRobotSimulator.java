0f9a745f60f671c35329a1be0a052c2b95e1270c
Jerome Savellano <jeromesavellano@Marielles-MacBook-Air.local>
2022-08-21 12:56:33 +0800

    Initial commit

---
+++ src/ToyRobotSimulator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ToyRobotSimulator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String commandStr;
+        while (!(commandStr = scanner.nextLine().toUpperCase()).equals("EXIT")) {
+        }
+    }
+}
